faef6541a80fed969b61325200c6b38a7c086744
Heyjjjj <89030158+Heyjjjj@users.noreply.github.com>
2026-09-20 14:41:31 +0800

    Delete AI directory

--- AI/middleware.py
+++
@@ -1,9 +0,0 @@
-from langchain.agents import create_agent
-from langchain.agents.middleware import *
-from langchain_community.chat_models.tongyi import ChatTongyi
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.tools import tool
-
-@before_agent
-def before_agent_fn(input):
-    print("before_agent:", input)
